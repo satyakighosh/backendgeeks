87c89d464cf80106b4beb967317c82549f213dcc
satyakighosh <satyaki1997ghosh@gmail.com>
2022-04-23 11:53:02 +0530

    koa graphql update

--- koaGraphql.ts
+++ koaGraphql.ts
@@ -12,5 +12,25 @@
 // graphql:
 // 1. resolver
 // 2. graphql server
+import * as koa from "koa";
+import { buildSchema } from "type-graphql";
+import hotelsResolver from "./hotelResolver";
+import { ApolloServer } from "apollo-server-koa";
+import * as Router from "koa-router";
+import {graphqlHTTP} from "koa-graphql";
+import "./createConnection.ts";
+import * as cors from "@koa/cors";
 
+async function main() {
+    const app = new koa();
+    const resolvers = await buildSchema({ resolvers: [hotelsResolver] });
+    const apolloServer = new ApolloServer({ schema: resolvers });
+    const router = new Router();
+    router.all("/graphql", graphqlHTTP({schema: resolvers}));
+    apolloServer.applyMiddleware({app});
+    app.use(router.routes());
+    app.use(cors());
+    app.listen(process.env.PORT || 3005, () => console.log)
+}
 
+main();
